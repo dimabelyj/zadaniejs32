8c17290c5f4ec58b737b69067e34c7148c1d0cbe
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-03-29 19:46:44 +0100

    dziła

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ let num2 = 5;
 
 function por(num1, num2) {
     if (num1 > num2) {
-        console.log('num1');
+        console.log(num1);
     } else if (num1 < num2) {
-        console.log(num2)
+        console.log(num2);
+        return por;
     }
 }
