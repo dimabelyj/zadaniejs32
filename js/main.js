23cea512307610610c1f324573279dbc36d6590b
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-02 17:02:51 +0200

    działa

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ let num2 = 5;
 function por(num1, num2) {
     if (num1 > num2) {
         console.log(num1);
+        return (num1, num2);
     } else if (num1 < num2) {
         console.log(num2);
-        return por;
+        return (num1, num2);
     }
 }
+por(num1, num2);
